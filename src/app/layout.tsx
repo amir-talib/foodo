bd45da9bb59d4ee8f3e8263304e8fcffae8917b9
Amir Talib <amirtalib@Amirs-MacBook-Pro.local>
2025-05-02 20:18:05 +0100

    feat: add favicon link to head for browser tab icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </head>
       <body>
         <ClientBody>{children}</ClientBody>
         <Analytics />
