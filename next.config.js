82c561e7e3363d5d8ab5ebe3fc6a772f21260871
Amir Talib <amirtalib@Amirs-MacBook-Pro.local>
2025-05-02 19:20:48 +0100

    chore: add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,47 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
   images: {
     unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "source.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ext.same-assets.com",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  basePath: '/foodo',
-  assetPrefix: '/foodo',
+  basePath: '',
+  assetPrefix: '',
   trailingSlash: true,
+  distDir: 'out',
 };
 
 module.exports = nextConfig;
